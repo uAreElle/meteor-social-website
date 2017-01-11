1db67e4ada1835601fbbd4bd82fa536d5bab8b31
Michelle Nguyen <uareelle@gmail.com>
2017-01-11 00:47:37 -0800

    Added user authentication and enable website posting

--- siteace.js
+++ siteace.js
@@ -1,3 +1,6 @@
+
+
+
 Websites = new Mongo.Collection("websites");
 
 if (Meteor.isClient) {
@@ -25,6 +28,8 @@ if (Meteor.isClient) {
 			var website_id = this._id;
 			console.log("Up voting website with id "+website_id);
 			// put the code in here to add a vote to a website!
+			//var rating = $(event.currentTarget).data("userrating");
+			//console.log("Website: "+website_id+" rating now: "+rating);
 
 			return false;// prevent the button from reloading the page
 		}, 
@@ -45,17 +50,48 @@ if (Meteor.isClient) {
 		"click .js-toggle-website-form":function(event){
 			$("#website_form").toggle('slow');
 		}, 
+		// when someone submits form with js-save-website-form class..
 		"submit .js-save-website-form":function(event){
 
 			// here is an example of how to get the url out of the form:
 			var url = event.target.url.value;
 			console.log("The url they entered is: "+url);
 			
+			// get title and description out of form:
+			var title, description;
+			title = event.target.title.value;
+			description = event.target.description.value;
+
 			//  put your website saving code in here!	
+			if(Meteor.user()){ // user logged in
+				Websites.insert({
+					url:url,
+					title:title,
+					description:description,
+				})
+			}
 
 			return false;// stop the form submit from reloading the page
 
 		}
+	});
+
+	// helper function to provide username
+	Template.body.helpers({username:function(){
+		if(Meteor.user()){ //user logged in
+			return Meteor.user().username;
+			// provides e-mail
+			//return Meteor.user().emails[0].address;
+		}
+		else {
+			return "anonymous";
+		}
+	}
+	});
+
+	// user username instead of e-mail when greeting
+	Accounts.ui.config({	// adds username field to signup form
+		passwordSignupFields: "USERNAME_AND_EMAIL"
 	});
 }
 
